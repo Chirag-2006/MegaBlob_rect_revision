9994c79434d7afe7f02ab2bbb00facdcf77a03bc
Chirag Arya <carya6785@gmail.com>
2026-06-17 23:55:42 +0530

    feat: add StorageServices for file management with Appwrite

---
+++ src/appwrite/storage.js
@@ -0,0 +1,50 @@
+// file upload service class
+// file delete service class
+// file preview service class
+
+import config from "../config/config.js";
+import { Client, Storage, ID } from "appwrite";
+
+export class StorageServices {
+  client = new Client();
+  storage;
+
+  constructor() {
+    this.client = this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectId);
+
+    this.storage = new Storage(this.client);
+  }
+
+  async fileUpload(file) {
+    try {
+      return await this.storage.createFile(
+        config.appwriteBucketId,
+        ID.unique(),
+        file,
+      );
+    } catch (error) {
+      console.error("appwrite Error :: fileUpload :", error);
+      throw error;
+    }
+  }
+
+  async deleteFile(fileId) {
+    try {
+      await this.storage.deleteFile(config.appwriteBucketId, fileId);
+      return true;
+    } catch (error) {
+      console.error("appwrite Error :: deleteFile :", error);
+      return false;
+    }
+  }
+
+  filePreview(fileId) {
+    return this.storage.getFilePreview(config.appwriteBucketId, fileId);
+  }
+}
+
+const storageServices = new StorageServices();
+
+export default storageServices;
