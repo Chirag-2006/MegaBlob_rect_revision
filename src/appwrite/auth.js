c09b8054fc6aecc08400c466b2d3e6a19cd45059
Chirag Arya <carya6785@gmail.com>
2026-06-17 17:58:21 +0530

    feat: implement AuthServices for user authentication with Appwrite

---
+++ src/appwrite/auth.js
@@ -0,0 +1,71 @@
+import config from "../config/config.js";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthServices {
+  client = new Client();
+  account;
+  constructor() {
+    this.client = this.client
+      .setProject(config.appwriteProjectId)
+      .setEndpoint(config.appwriteUrl);
+
+    this.account = new Account(this.client);
+  }
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name,
+      );
+
+      if (userAccount) {
+        // call login function for this user
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      console.error("appwrite Error :: createAccount :", error);
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      const user = await this.account.createEmailPasswordSession({
+        email,
+        password,
+      });
+      if (!user) {
+        console.log("failed to login user");
+      }
+      return user;
+    } catch (error) {
+      console.error("appwrite Error :: login :", error);
+      throw error;
+    }
+  }
+
+  async logout() {
+    try {
+      return await this.account.deleteSessions();
+    } catch (error) {
+      console.error("appwrite Error :: logout :", error);
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      const user = await this.account.get();
+      return user;
+    } catch (error) {
+      console.error("appwrite Error :: getCurrentUser :", error);
+      throw error;
+    }
+  }
+}
+
+const authService = new AuthServices();
+
+export default authService;
