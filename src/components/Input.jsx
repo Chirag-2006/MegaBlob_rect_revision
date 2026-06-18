e084cfb5898c33d361c7569f7c2524e75403e96a
Chirag Arya <carya6785@gmail.com>
2026-06-18 14:37:01 +0530

    feat: add Input component for reusable input field functionality

---
+++ src/components/Input.jsx
@@ -0,0 +1,24 @@
+import { useId } from "react";
+
+function Input({ label, type = "text",ref, className = "", ...props }) {
+
+  const id = useId();
+  return (
+    <div>
+      {label && (
+        <label htmlFor={id} className="inline-block ml-1 pl-1">
+          {label}
+        </label>
+      )}
+      <input
+        type={type}
+        className={`w-full px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-200 border border-gray-200  ${className}`}
+        ref={ref}
+        {...props}
+        id={id}
+      />
+    </div>
+  );
+}
+
+export default Input;
