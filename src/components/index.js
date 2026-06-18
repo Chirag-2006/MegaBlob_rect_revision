0e9378087cf560d32fe24984861c19e7f5b32d0f
Chirag Arya <carya6785@gmail.com>
2026-06-18 15:40:19 +0530

    feat: add Input component to index export for improved modularity

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,9 @@
+import Buttton from "./Buttton";
 import Container from "./container/container";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Input";
 import Logo from "./Logo";
 
-
-export { Header, Footer, Container, Logo, LogoutBtn };
+export { Header, Footer, Container, Logo, LogoutBtn, Buttton, Input };
