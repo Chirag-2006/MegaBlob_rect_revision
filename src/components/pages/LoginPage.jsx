3625febff293c60305749e641869bb65d739b7f9
Chirag Arya <carya6785@gmail.com>
2026-06-21 20:02:46 +0530

    feat: add LoginPage component for user authentication

---
+++ src/components/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import { Login as LoginComponent } from "../index";
+
+function LoginPage() {
+  return (
+    <div className="py-8">
+      <LoginComponent />
+    </div>
+  );
+}
+
+export default LoginPage;
