5cf631e61f0c9ddb793382d77f4b81153153b7c8
Chirag Arya <carya6785@gmail.com>
2026-06-21 22:19:51 +0530

    refactor: simplify routing structure and clean up commented code in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -21,68 +21,171 @@ import {
 } from "./pages/";
 import { AuthLayout } from "./components/index.js";
 
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<App />}>
+//       <Route
+//       index
+//         // path="/"
+//         element={
+//           <AuthLayout authentication={false}>
+//             <Home />
+//           </AuthLayout>
+//         }
+//       />
+//       <Route
+//         path="/login"
+//         element={
+//           <AuthLayout authentication={false}>
+//             <LoginPage />
+//           </AuthLayout>
+//         }
+//       />
+//       <Route
+//         path="/signup"
+//         element={
+//           <AuthLayout authentication={false}>
+//             <SignUpPage />
+//           </AuthLayout>
+//         }
+//       />
+//       <Route
+//         path="/all-post"
+//         element={
+//           <AuthLayout authentication>
+//             <AllPosts />
+//           </AuthLayout>
+//         }
+//       />
+//       <Route
+//         path="/add-post"
+//         element={
+//           <AuthLayout authentication={true}>
+//             <AddPost />
+//           </AuthLayout>
+//         }
+//       />
+//       <Route
+//         path="/edit-post/:id"
+//         element={
+//           <AuthLayout authentication={true}>
+//             <EditPost />
+//           </AuthLayout>
+//         }
+//       />
+//       <Route
+//         path="/post/:id"
+//         element={
+//           <AuthLayout authentication={true}>
+//             <Post />
+//           </AuthLayout>
+//         }
+//       />
+//     </Route>,
+//   ),
+// );
+
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      <Route
-        path="/"
-        element={
-          <AuthLayout authentication={false}>
-            <Home />
-          </AuthLayout>
-        }
-      />
-      <Route
-        path="/login"
-        element={
-          <AuthLayout authentication={false}>
-            <LoginPage />
-          </AuthLayout>
-        }
-      />
-      <Route
-        path="/signup"
-        element={
-          <AuthLayout authentication={false}>
-            <SignUpPage />
-          </AuthLayout>
-        }
-      />
-      <Route
-        path="/all-post"
-        element={
-          <AuthLayout authentication>
-            <AllPosts />
-          </AuthLayout>
-        }
-      />
-      <Route
-        path="/add-post"
-        element={
-          <AuthLayout authentication={true}>
-            <AddPost />
-          </AuthLayout>
-        }
-      />
-      <Route
-        path="/edit-post/:id"
-        element={
-          <AuthLayout authentication={true}>
-            <EditPost />
-          </AuthLayout>
-        }
-      />
-      <Route
-        path="/post/:id"
-        element={
-          <AuthLayout authentication={true}>
-            <Post />
-          </AuthLayout>
-        }
-      />
-    </Route>,
-  ),
+      <Route index element={
+        <AuthLayout authentication={false}>
+          <Home />
+        </AuthLayout>
+      } />
+      <Route path="login" element={
+        <AuthLayout authentication={false}>
+          <LoginPage />
+        </AuthLayout>
+      } />
+      <Route path="signup" element={
+        <AuthLayout authentication={false}>
+          <SignUpPage />
+        </AuthLayout>
+      } />
+      <Route path="all-post" element={
+        <AuthLayout authentication={true}>
+          <AllPosts />
+        </AuthLayout>
+      } />
+      <Route path="add-post" element={
+        <AuthLayout authentication={true}>
+          <AddPost />
+        </AuthLayout>
+      } />
+      <Route path="edit-post/:id" element={
+        <AuthLayout authentication={true}>
+          <EditPost />
+        </AuthLayout>
+      } />
+      <Route path="post/:id" element={
+        <AuthLayout authentication={true}>
+          <Post />
+        </AuthLayout>
+      } />
+    </Route>
+  )
 );
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <Home />,
+//       },
+//       {
+//         path: "/login",
+//         element: (
+//           <AuthLayout authentication={false}>
+//             <LoginPage />
+//           </AuthLayout>
+//         ),
+//       },
+//       {
+//         path: "/signup",
+//         element: (
+//           <AuthLayout authentication={false}>
+//             <SignUpPage />
+//           </AuthLayout>
+//         ),
+//       },
+//       {
+//         path: "/all-posts",
+//         element: (
+//           <AuthLayout authentication>
+//             {" "}
+//             <AllPosts />
+//           </AuthLayout>
+//         ),
+//       },
+//       {
+//         path: "/add-post",
+//         element: (
+//           <AuthLayout authentication>
+//             {" "}
+//             <AddPost />
+//           </AuthLayout>
+//         ),
+//       },
+//       {
+//         path: "/edit-post/:slug",
+//         element: (
+//           <AuthLayout authentication>
+//             {" "}
+//             <EditPost />
+//           </AuthLayout>
+//         ),
+//       },
+//       {
+//         path: "/post/:slug",
+//         element: <Post />,
+//       },
+//     ],
+//   },
+// ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
