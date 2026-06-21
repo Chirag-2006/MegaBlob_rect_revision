a15889e48a228eb82823a0b1792552550b7fae64
Chirag Arya <carya6785@gmail.com>
2026-06-21 16:01:31 +0530

    feat: add Select and RTE components to index export

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,7 @@ import Header from "./Header/Header";
 import LogoutBtn from "./Header/LogoutBtn";
 import Input from "./Input";
 import Logo from "./Logo";
+import RTE from "./RTE";
+import Select from './Select'
 
-export { Header, Footer, Container, Logo, LogoutBtn, Buttton, Input };
+export { Header, Footer, Container, Logo, LogoutBtn, Buttton, Input , Select,RTE};
