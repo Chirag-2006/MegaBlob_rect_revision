f698aa29443cdce0b9295d3aa1590f5a8f271242
Chirag Arya <carya6785@gmail.com>
2026-06-21 22:04:50 +0530

    fix: uncomment Outlet import and adjust console log formatting in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
-import "./App.css";
+// import "./App.css";
 import { useDispatch } from "react-redux";
 import { login, logout } from "./store/authSlice";
 import authService from "./appwrite/auth";
 import { Header, Footer } from "./components";
-// import { Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -16,7 +16,7 @@ function App() {
       .getCurrentUser()
       .then((userData) => {
         if (userData) {
-          console.log("userData in app.jsx",userData)
+          console.log("userData in app.jsx", userData);
           dispatch(login(userData));
         } else {
           dispatch(logout());
@@ -35,7 +35,9 @@ function App() {
       <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
         <div className="w-full block">
           <Header />
-          <main>{/* <Outlet /> */}</main>
+          <main>
+            <Outlet />{" "}
+          </main>
           <Footer />
         </div>
       </div>
