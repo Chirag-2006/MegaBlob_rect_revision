df562a23521e3a9ed07578e2bbbf59d59ca74e2d
Chirag Arya <carya6785@gmail.com>
2026-06-24 19:09:14 +0530

    feat: implement post management async thunks for CRUD operations

---
+++ src/store/postThunk.js
@@ -0,0 +1,59 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import dbServices from "../appwrite/database";
+
+export const fetchPosts = createAsyncThunk(
+  "post/fetchPosts",
+  async (_, thunkAPI) => {
+    try {
+      const response = await dbServices.getPosts();
+      return response.rows;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
+);
+
+export const fetchPostById = createAsyncThunk(
+  "post/fetchPostById",
+  async (id, thunkAPI) => {
+    try {
+      return await dbServices.getPostById(id);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
+);
+
+export const createPost = createAsyncThunk(
+  "post/createPost",
+  async (data, thunkAPI) => {
+    try {
+      return await dbServices.createPost();
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
+);
+
+export const updatePost = createAsyncThunk(
+  "post/updatePost",
+  async ({ id, data }, thunkAPI) => {
+    try {
+      return await dbServices.updatePost(id, data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
+);
+
+export const deletePost = createAsyncThunk(
+  "post/deletePost",
+  async (id, thunkAPI) => {
+    try {
+      await dbServices.deletePost(id);
+      return id;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
+);
